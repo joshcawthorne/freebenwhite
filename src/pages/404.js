9cecd5322da429b7ca15f5c1a445e7a28cc2d0f9
Josh Cawthorne <joshcawthorne97@gmail.com>
2020-08-09 19:13:13 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,32 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #19213a;
+  color: #fff;
+  text-align: center;
+  @media (max-width: 768px) {
+    height: 100%;
+    min-height: 100vh;
+    min-height: webkit-fill-available;
+  }
+`;
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1>NOT FOUND</h1>
+    </Container>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
